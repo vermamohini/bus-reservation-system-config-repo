8b309f8cae4c8602205bf31dbd21c3fe3fe682a5
Sawarni Manu <sawarni.manu@gmail.com>
2023-09-09 15:13:23 +0530

    DB script added and Configuration file for payment service

---
+++ db_script.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE bus_reservation_db;
+USE bus_reservation_db;
+CREATE TABLE bus_route (
+  bus_number VARCHAR(10) PRIMARY KEY,
+  start_point VARCHAR(500) NOT NULL,
+  end_point VARCHAR(500) NOT NULL,
+  price_per_ticket DOUBLE(5,2) NOT NULL,
+  total_seats INT NOT NULL
+);
+
+CREATE TABLE bus_inventory (
+  bus_number VARCHAR(10) PRIMARY KEY,
+  available_seats INT NOT NULL,
+  last_updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  foreign key(bus_number) references bus_route(bus_number)
+);
+
+CREATE TABLE booking_details (
+  booking_number INT PRIMARY KEY,
+  bus_number VARCHAR(10) NOT NULL,
+  start_point VARCHAR(500) NOT NULL,
+  end_point VARCHAR(500) NOT NULL,
+  no_of_seats INT NOT NULL,
+  booking_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  foreign key(bus_number) references bus_route(bus_number)
+);
+
+CREATE TABLE booking_status (
+  status_id INT PRIMARY KEY,
+  booking_number INT NOT NULL,
+  booking_status VARCHAR(50) NOT NULL,
+  created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  foreign key(booking_number) references booking_details(booking_number)
+);
+
+CREATE TABLE passenger_details (
+  passenger_id INT PRIMARY KEY,
+  booking_number INT NOT NULL,
+  foreign key(booking_number) references booking_details(booking_number)
+);
+
+
+CREATE TABLE payment_details (
+  payment_number INT PRIMARY KEY,
+  booking_number INT NOT NULL,
+  payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  price DOUBLE(5,2) NOT NULL,
+  foreign key(booking_number) references booking_details(booking_number)
+);
